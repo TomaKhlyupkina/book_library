301cf24c8edf6fa3fb865a47acc0fa769323e2fa
Toma Khlyupkina <toma99@mail.ru>
2022-07-15 14:13:01 +0300

    Writing data to the file was implemented

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,24 @@ import "./App.css";
 
 function App() {
     const [data, setData] = React.useState(null);
+    const [book, setBook] = React.useState({name: "", author: "", date: ""})
+
+    const addBookToDB = (e) => {
+        e.preventDefault()
+        let response = fetch("/add_book", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(book)
+        })
+        response.then((res) => {
+            if (!res.ok) {
+                console.log("ERROR: " + res.statusText)
+            }
+        })
+        setBook({name: "", author: "", date: ""})
+    }
 
     React.useEffect(() => {
         fetch("/api")
@@ -13,6 +31,24 @@ function App() {
     return (
         <div>
             <header>
+                <form>
+                    <input
+                        placeholder={"Book's name"}
+                        value={book.name}
+                        onChange={e => setBook({...book, name: e.target.value})}
+                    />
+                    <input
+                        placeholder={"Author"}
+                        value={book.author}
+                        onChange={e => setBook({...book, author: e.target.value})}
+                    />
+                    <input
+                        placeholder={"Publication date"}
+                        value={book.date}
+                        onChange={e => setBook({...book, date: e.target.value})}
+                    />
+                    <button onClick={addBookToDB}>Add book</button>
+                </form>
                 <p>{!data ? "Loading..." : data}</p>
             </header>
         </div>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,36 @@
 const express = require("express");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json())
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
+
+let books = []
+
+const addAllBooksToDb = () => {
+    fs.writeFile("./database/books.json", JSON.stringify(books), err => {
+        if (err) {
+            console.error(err);
+        }}
+    )
+}
+
+app.post("/add_book", (req, res) => {
+    if (!req.body) {
+        res.status(500).end();
+        return
+    }
+
+    books.push(req.body)
+    addAllBooksToDb()
+
+    res.status(200).end()
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
